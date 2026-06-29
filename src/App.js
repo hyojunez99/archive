4549d3ec34a67398bb6c6d0c7daf57ca299cc7fa
hyojunez99 <hyojuez99@gmail.com>
2026-06-29 10:00:47 +0900

    2026.06.29-3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from "./components/common/footer/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/archive">
       <Header />
 
       <Routes>
